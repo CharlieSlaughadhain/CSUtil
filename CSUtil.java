bc525386466696d6d5f928c2cc6cb420a2595702
CharlieSlaughadhain <charlesjsloan91@gmail.com>
2020-07-29 11:52:59 +1000

    Added currency capacity up to 11 integers

--- CSUtil.java
+++ CSUtil.java
@@ -18,14 +18,15 @@ public class CSUtil{
         }
     
     // Trim excess decimals and round to correct number (1.5 rounds to 2 and 
-        int LNumber = (int) (number * p * 10);
-        int director = LNumber % 10;
+        long LNumber = (long) (number * p * 10);
+        long mod = LNumber % 10L;
+        int director = (int) mod;
         LNumber /= 10;
         if (director >= 5) LNumber++;
         double FNumber = (double) LNumber / p;
 
     // Convert Double to String
-        String str = new String("" + FNumber);
+        String str = doubleToString(FNumber);
 
     // Ensure all zeros are shown
         if (places > 0 && includeZeros){
@@ -47,5 +48,72 @@ public class CSUtil{
 
     // Return the generated string for ouput
         return str;
+    }
+
+
+// Convert Double to a String
+    public static String doubleToString(double d){
+
+        // Find the length of the integral part.
+        Long l = Long.valueOf(1);
+        int len = 0;
+        while(d > l){
+            l *= 10;
+            len++;
+        }
+        int intLen = len;
+
+        // Rewrite as a string.
+        int count = len - 3;
+        String result = "";
+        int letter;
+        for (int i = 0; i < count; i++){
+            l /= 10;
+            letter = (int) (d / l);
+            result += letter;
+            d -= l * letter;
+        }
+        result += (float) d;
+
+        return result;
+    }
+
+    public static String toCurrency(double value, String currency){ return formatString(trunc(value, 2, true), currency); }
+
+    public static String formatString(double number, String prefix){ return formatString(String.valueOf(number), prefix); }
+
+    public static String formatString(double number){ return formatString(String.valueOf(number)); }
+    
+    public static String formatString(String num){ return formatString(num, ""); }
+
+    public static String formatString(String num, String prefix){
+
+        int x = num.indexOf(".");
+        int div = x / 3;
+        int   r = x % 3;
+
+        String tail = "";
+        for (int i = x; i < num.length(); i++) {
+            tail += num.charAt(i);
+        }
+
+        int point = 0;
+        String str = prefix;
+
+        for (int i = 0; i < r; i++) {
+            str += num.charAt(point++);
+        }
+
+        for (int i = 0; i < div; i++){
+
+            if (r != 0) str += ",";
+            r++;
+
+            for (int j = 0; j < 3; j++){
+                str += num.charAt(point++);
+            }
+        }
+
+        return str + tail;
     }
 }
--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 
 public class Test {
     public static void main(String[] args) {
-        double tester = 50.5;
-        System.out.println(CSUtil.trunc(tester, 3, false));
+        
+        double tester = 99999999999.99; // Good for up too 11 digits
+
+        System.out.println(CSUtil.toCurrency(tester, "$"));
     }
 }
